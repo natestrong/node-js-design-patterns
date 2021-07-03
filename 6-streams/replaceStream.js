182c80f914fc1d239a7cc20b75e1123331b54fc3
nstrong <nathanstrong.fx@gmail.com>
2021-07-02 19:28:11 -0700

    replaceStream

--- 6-streams/replaceStream.js
+++ 6-streams/replaceStream.js
@@ -1,56 +1,57 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 exports.__esModule = true;
-exports.ReplaceStream = void 0;
 var stream_1 = require("stream");
-var ReplaceStream = /** @class */ (function (_super) {
-    __extends(ReplaceStream, _super);
-    function ReplaceStream(searchStr, replaceStr, options) {
-        var _this = _super.call(this, __assign({}, options)) || this;
-        _this.searchStr = searchStr;
-        _this.replaceStr = replaceStr;
-        _this.tail = '';
-        return _this;
-    }
-    ReplaceStream.prototype._transform = function (chunk, encoding, callback) {
-        var pieces = (this.tail + chunk).split(this.searchStr); // (1)
-        var lastPiece = pieces[pieces.length - 1]; // (2)
-        var tailLen = this.searchStr.length - 1;
-        this.tail = lastPiece.slice(-tailLen);
+// export class ReplaceStream extends Transform {
+//     private readonly searchStr: any;
+//     private readonly replaceStr: any;
+//     private tail: string;
+//
+//     constructor(searchStr, replaceStr, options = {}) {
+//         super({...options});
+//         this.searchStr = searchStr;
+//         this.replaceStr = replaceStr;
+//         this.tail = '';
+//     }
+//
+//     _transform(chunk, encoding, callback) {
+//         const pieces = (this.tail + chunk).split(this.searchStr);  // (1)
+//         const lastPiece = pieces[pieces.length - 1];               // (2)
+//         const tailLen = this.searchStr.length - 1;
+//         this.tail = lastPiece.slice(-tailLen);
+//         pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen);
+//         this.push(pieces.join(this.replaceStr));                   // (3)
+//         callback();
+//     }
+//
+//     _flush(callback) {
+//         this.push(this.tail);
+//         callback();
+//     }
+// }
+//
+// const replaceStream = new ReplaceStream('World', 'Node.js');
+// Same API with simplified construction
+var searchStr = 'World';
+var replaceStr = 'Node.js';
+var tail = '';
+var replaceStream = new stream_1.Transform({
+    defaultEncoding: 'utf8',
+    transform: function (chunk, encoding, callback) {
+        var pieces = (tail + chunk).split(searchStr);
+        var lastPiece = pieces[pieces.length - 1];
+        var tailLen = searchStr.length - 1;
+        tail = lastPiece.slice(-tailLen);
         pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen);
-        this.push(pieces.join(this.replaceStr)); // (3)
+        this.push(pieces.join(replaceStr));
         callback();
-    };
-    ReplaceStream.prototype._flush = function (callback) {
-        this.push(this.tail);
+    },
+    flush: function (callback) {
+        this.push(tail);
         callback();
-    };
-    return ReplaceStream;
-}(stream_1.Transform));
-exports.ReplaceStream = ReplaceStream;
+    }
+});
+replaceStream.on('data', function (chunk) { return console.log(chunk.toString()); });
+replaceStream.write('Hello W');
+replaceStream.write('orld!');
+replaceStream.end();
 //# sourceMappingURL=replaceStream.js.map
--- 6-streams/replaceStream.ts
+++ 6-streams/replaceStream.ts
@@ -1,31 +1,60 @@
-import {Transform} from "stream";
+import {Transform, TransformCallback} from "stream";
+import {last} from "rxjs/operators";
 
-export class ReplaceStream extends Transform {
-    private readonly searchStr: any;
-    private readonly replaceStr: any;
-    private tail: string;
+// export class ReplaceStream extends Transform {
+//     private readonly searchStr: any;
+//     private readonly replaceStr: any;
+//     private tail: string;
+//
+//     constructor(searchStr, replaceStr, options = {}) {
+//         super({...options});
+//         this.searchStr = searchStr;
+//         this.replaceStr = replaceStr;
+//         this.tail = '';
+//     }
+//
+//     _transform(chunk, encoding, callback) {
+//         const pieces = (this.tail + chunk).split(this.searchStr);  // (1)
+//         const lastPiece = pieces[pieces.length - 1];               // (2)
+//         const tailLen = this.searchStr.length - 1;
+//         this.tail = lastPiece.slice(-tailLen);
+//         pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen);
+//         this.push(pieces.join(this.replaceStr));                   // (3)
+//         callback();
+//     }
+//
+//     _flush(callback) {
+//         this.push(this.tail);
+//         callback();
+//     }
+// }
+//
+// const replaceStream = new ReplaceStream('World', 'Node.js');
 
-    constructor(searchStr, replaceStr, options) {
-        super({...options});
-        this.searchStr = searchStr;
-        this.replaceStr = replaceStr;
-        this.tail = '';
-    }
-
-    _transform(chunk, encoding, callback) {
-        const pieces = (this.tail + chunk).split(this.searchStr);  // (1)
-        const lastPiece = pieces[pieces.length - 1];               // (2)
-        const tailLen = this.searchStr.length - 1;
-        this.tail = lastPiece.slice(-tailLen);
+// Same API with simplified construction
+const searchStr = 'World';
+const replaceStr = 'Node.js';
+let tail = '';
+const replaceStream = new Transform({
+    defaultEncoding: 'utf8',
+    transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback) {
+        const pieces = (tail + chunk).split(searchStr);
+        const lastPiece = pieces[pieces.length - 1];
+        const tailLen = searchStr.length - 1;
+        tail = lastPiece.slice(-tailLen);
         pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen);
-        this.push(pieces.join(this.replaceStr));                   // (3)
+        this.push(pieces.join(replaceStr));
+        callback();
+    },
+    flush(callback: TransformCallback) {
+        this.push(tail);
         callback();
     }
+});
 
-    _flush(callback) {
-        this.push(this.tail);
-        callback();
-    }
-}
 
+replaceStream.on('data', chunk => console.log(chunk.toString()));
+replaceStream.write('Hello W');
+replaceStream.write('orld!');
+replaceStream.end();
 
