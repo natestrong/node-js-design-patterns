7f3bb95c7aea75e9b1561087330a96b133ebb116
nstrong <nathanstrong.fx@gmail.com>
2021-07-13 18:51:41 -0700

    Generators

---
+++ 8-structuralDesignPatterns/checkUrls.ts
@@ -0,0 +1,39 @@
+import superagent from 'superagent';
+
+export class CheckUrls {
+    private readonly urls: string[];
+
+    constructor(urls) {                                             // (1)
+        this.urls = urls;
+    }
+
+    [Symbol.asyncIterator]() {
+        const urlsIterator = this.urls[Symbol.iterator]();          // (2)
+        return {
+            async next() {                                          // (3)
+                const iteratorResult = urlsIterator.next();         // (4)
+                if (iteratorResult.done) {
+                    return {done: true};
+                }
+                const url = iteratorResult.value;
+                try {
+                    const checkResult = await superagent           // (5)
+                        .head(url)
+                        .redirects(2);
+                    return {
+                        done: false,
+                        value: `${url} is up, status: ${checkResult.status}`
+                    };
+                } catch (err) {
+                    return {
+                        done: false,
+                        value: `${url} is down, error: ${err.message}`
+                    };
+                }
+            }
+        };
+    }
+}
+
+
+
